24b10ce334602a4ac8028e2e526012ce2ea742c6
LPMOD <mx.gamer.in@gmail.com>
2026-01-14 23:03:29 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export async function handler() {
   try {
     const url =
-      "https://proapi.sumittools.shop/emote" +
+      "https://proapi.sumittools.shop/emoe" +
       "?key=ShadowProTCP" +
       "&region=ind" +
       "&tc=9807987" +
