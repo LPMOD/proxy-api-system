9a21a972dea61e9c9476dd019e1585fc3dac49ac
LPMOD <mx.gamer.in@gmail.com>
2026-01-14 22:33:00 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,1 +1,14 @@
+let logs = [];
 
+export function saveLog(data) {
+  logs.push(data);
+
+  // sirf last 300 request rakhenge
+  if (logs.length > 300) {
+    logs.shift();
+  }
+}
+
+export function getLogs() {
+  return logs;
+}
